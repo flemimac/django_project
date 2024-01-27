6b349eda09efffc0363eef94b771337190119a44
flemimac <64695348+flemimac@users.noreply.github.com>
2024-01-28 00:07:46 +0200

    add app2 in templates and function in views.py(app2)

--- DJDjango/DJDjango/urls.py
+++ DJDjango/DJDjango/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app.views import index, contacts, about
+from app2.views import help
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('contacts/', contacts),
     path('about/', about),
+    path('help/', help),
 ]
